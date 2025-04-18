4cbc2c3aac5ee79af4aa3b9593c0dad3099efc89
federico-paganini <perfedefer@gmail.com>
2025-04-18 20:21:53 +0000

    [FEAT] Input processing functions

--- input_handler.c
+++ input_handler.c
@@ -21,7 +21,7 @@ char *get_line(void)
 
 	nread = getline(&line, &len, stdin);
 
-	if ((strcmp(line, "exit\n") == 0) || nread = -1)
+	if ((strcmp(line, "exit\n") == 0) || nread == -1)
 	{
 		if (interactive)
 		{
@@ -32,7 +32,7 @@ char *get_line(void)
 		exit(EXIT_SUCCESS);
 	}
 
-	if (nread > 0 && line[nread-1] == '\n')
+	if (nread > 0 && line[nread - 1] == '\n')
 		line[nread - 1] = '\0';
 
 	if (*line == '\0')
@@ -46,3 +46,66 @@ char *get_line(void)
 
 char **check_validity(char *line)
 {
+	char **args;
+
+	if (line == NULL)
+		return (NULL);
+
+	while (line[i] != '\0' && line[i] == ' ' || line[i] == '\t')
+		i++;
+
+	if (line[i] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
+
+	tokenize_line(line);
+
+	if (args == NULL)
+		return (NULL);
+
+	free(line);
+
+	return (args);
+}
+
+char **tokenize_line(char *line)
+{
+	char **args = malloc(sizeof(char *) * (arg_count(line) + 1));
+	int i = 0, j;
+	int word = 0;
+
+	if (args == NULL)
+		return (NULL);
+
+	while (line[i] != '\0')
+	{
+		if (line[i] == ' ' || line[i] == '\t')
+		{
+			i++;
+			continue;
+		}
+
+		args[word] = malloc(sizeof(char) * (_wlen(&line[i]) + 1));
+		if (args[word] == NULL)
+		{
+			for (i = 0; i < word; i++)
+				free(args[i]);
+			free(args);
+			return (NULL);
+		}
+
+		j = 0;
+
+		while (line[i] != ' ' && line[i] != '\t' && line[i] != '\0')
+			args[word][j++] = line[i++];
+
+		args[word][j] = '\0';
+		word++;
+	}
+
+	args[word] = NULL;
+
+	return (args);
+}
