997441fe6e99a7c37bf8457d49861c24e6cbd787
federico-paganini <perfedefer@gmail.com>
2025-04-18 23:58:09 +0000

    [FEAT] Interpreter functions

--- gates_of_shell.c
+++ gates_of_shell.c
@@ -10,19 +10,28 @@
  * Return: 0.
  */
 
-int main(int argc __attribute__((__unused__)), char **argv __attribute__((__unused__)), char **envp)
+int main(int argc __attribute__((__unused__)),
+		char **argv __attribute__((__unused__)), char **envp)
 {
-	char **args;
+	char **args = check_validity(get_line()), 
+	char **env = copy_envp(envp);
+	char *path;
 
 	for (;;)
 	{
 		args = check_validity(get_line());
 
 		if (args == NULL)
+			continue;
+
+		path = interpreter(args, env);
+
+		if (path == NULL)
 		{
+			free_all(args);
 			continue;
 		}
-
+		
 		free_all(args);
 	}
 	return (EXIT_SUCCESS);
--- interpreter.c
+++ interpreter.c
@@ -1,1 +1,54 @@
 #include "shell.h"
+
+char *interpreter(char **args, char **env)
+{
+	char *cmd = args[0];
+	char *path;
+	
+	if (args == NULL || cmd == NULL)
+		return (NULL);
+
+	if (cmd[0] == '/' || (cmd[0] == '.' && cmd[1] == '/'))
+	{
+		if (access(cmd, X_OK) == 0)
+		{
+			return(cmd);
+		}
+		else
+		{
+			perror(cmd);
+			return(NULL);
+		}
+	}
+
+	path = find_path(cmd, env);
+	if (path == NULL)
+	{
+		fprintf(stderr, "%s: command not found\n", args[0]);
+		return (NULL);
+	}
+
+	return (path);
+}
+
+char *find_path(char *cmd, char **env)
+{
+	char *path = getenv("PATH");
+	char **orgpath;
+	char *result;
+	char *full_path = NULL;
+
+	while (result != NULL)
+	{
+		result = strstr(aux, cmd);
+
+		if (result != NULL)
+			result += strlen(cmd);
+
+		if (*result == ':' || *result == '\0')
+			break;
+
+		aux = result;
+	}
+
+
